db0470549990d6161a0298a8c066b152f1520eba
Samson Kitigo <samsonjesuyon@gmail.com>
2025-02-06 11:35:01 -0600

    update

--- frontend/src/components/API.ts
+++ frontend/src/components/API.ts
@@ -1,3 +1,3 @@
 //Update your API according
-export const CREATE_CONTACT_URL = "https://imwpwvk6ih.execute-api.us-east-1.amazonaws.com/test";
-export const GET_CONTACTS_URL = "https://imwpwvk6ih.execute-api.us-east-1.amazonaws.com/test";
+export const CREATE_CONTACT_URL = "https://uyxusmnzx8.execute-api.us-east-1.amazonaws.com/dev/createContact";
+export const GET_CONTACTS_URL = "https://uyxusmnzx8.execute-api.us-east-1.amazonaws.com/dev/getContact";
